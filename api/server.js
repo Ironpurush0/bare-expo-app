b484181200ddaf991131cdbb10be1826c784a49a
Ironpurush0 <ridamsaxena90.com>
2020-11-24 21:17:10 +0530

    .

--- api/server.js
+++ api/server.js
@@ -7,10 +7,30 @@ const mogran = require('morgan')
 const {ExpressPeerServer} = require('peer')
 
 const app = express()
+const server = http.createServer(app)
+
+const io = socketio(server).sockets
 
 app.use(express.json())
 
 const customGenerationFunction = () => {
     (Math.random().toString(36) + "000000000000").substr(2, 16)
 }
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/',
+    generateClientId: customGenerationFunction
+})
+
+app.use('/mypeer', peerServer)
+
+io.on(connection, (socket) => {
+    console.log("Connected")
+})
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`Server is running on port ${port}`))
+
 
